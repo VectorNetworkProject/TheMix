0002cbe85bab68502d13351f19e3ce9b212f1661
InkoHX <inkohx@gmail.com>
2018-11-12 03:04:25 +0900

    Fix

--- src/VectorNetworkProject/TheMix/TheMix.php
+++ src/VectorNetworkProject/TheMix/TheMix.php
@@ -1,6 +1,9 @@
 <?php
 /**
  * Copyright (c) 2018 VectorNetworkProject. All rights reserved. MIT license.
+ *
+ * GitHub: https://github.com/VectorNetworkProject/TheMix
+ * Website: https://www.vector-network.tk
  */
 
 namespace VectorNetworkProject\TheMix;
