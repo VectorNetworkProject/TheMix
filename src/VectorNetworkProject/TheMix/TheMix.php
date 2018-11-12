4111c92cbbb49cec0e32efc3a621bdb56178016c
InkoHX <inkohx@gmail.com>
2018-11-13 03:33:36 +0900

    register events

--- src/VectorNetworkProject/TheMix/TheMix.php
+++ src/VectorNetworkProject/TheMix/TheMix.php
@@ -13,6 +13,7 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 use VectorNetworkProject\TheMix\command\PingCommand;
 use VectorNetworkProject\TheMix\command\TpsCommand;
+use VectorNetworkProject\TheMix\event\ThePlayerLoginEvent;
 
 class TheMix extends PluginBase
 {
@@ -28,6 +29,7 @@ class TheMix extends PluginBase
     public function onEnable()
     {
         $this->registerCommands();
+        $this->registerEvents();
         $this->getLogger()->notice(TextFormat::AQUA . '
 
 
@@ -72,5 +74,11 @@ class TheMix extends PluginBase
             new TpsCommand($this)
         ];
         $this->getServer()->getCommandMap()->registerAll($this->getName(), $commands);
+    }
+
+    private function registerEvents(): void
+    {
+        $plm = $this->getServer()->getPluginManager();
+        $plm->registerEvents(new ThePlayerLoginEvent(), $this);
     }
 }
