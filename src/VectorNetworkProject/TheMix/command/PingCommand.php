ad49150b5f2043eba305a129f9c53970d9b2d8e9
InkoHX <inkohx@gmail.com>
2018-11-13 02:27:54 +0900

    Add commands

---
+++ src/VectorNetworkProject/TheMix/command/PingCommand.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright (c) 2018 VectorNetworkProject. All rights reserved. MIT license.
+ *
+ * GitHub: https://github.com/VectorNetworkProject/TheMix
+ * Website: https://www.vector-network.tk
+ */
+
+namespace VectorNetworkProject\TheMix\command;
+
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginCommand;
+use pocketmine\Player;
+use pocketmine\plugin\Plugin;
+use pocketmine\utils\TextFormat;
+
+class PingCommand extends PluginCommand
+{
+    public function __construct(Plugin $owner)
+    {
+        parent::__construct('ping', $owner);
+        $this->setDescription('応答速度を計測します。');
+        $this->setPermission('the.mix.command.ping');
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool
+    {
+        if (!$sender instanceof Player) {
+            $sender->sendMessage(TextFormat::RED . "このコマンドはプレイヤーのみ実行可能です。");
+            return true;
+        }
+        $sender->sendMessage(TextFormat::RED . $sender->getPing() . TextFormat::YELLOW . "ms");
+        return true;
+    }
+}
---
+++ src/VectorNetworkProject/TheMix/command/TpsCommand.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright (c) 2018 VectorNetworkProject. All rights reserved. MIT license.
+ *
+ * GitHub: https://github.com/VectorNetworkProject/TheMix
+ * Website: https://www.vector-network.tk
+ */
+
+namespace VectorNetworkProject\TheMix\command;
+
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginCommand;
+use pocketmine\plugin\Plugin;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+
+class TpsCommand extends PluginCommand
+{
+    public function __construct(Plugin $owner)
+    {
+        parent::__construct('tps', $owner);
+        $this->setPermission('the.mix.command.tps');
+        $this->setDescription('TicksPerSecond');
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool
+    {
+        $sender->sendMessage(TextFormat::GREEN . 'TPS: ' . Server::getInstance()->getTicksPerSecond() . '/20');
+        return true;
+    }
+}
