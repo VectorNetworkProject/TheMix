fd698862e2d7adaccf5394c9d9cf849ce77afe72
InkoHX <inkohx@gmail.com>
2018-11-16 04:12:22 +0900

    Add PlayerXpChangeEvent

---
+++ src/VectorNetworkProject/TheMix/event/game/PlayerXpChangeEvent.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright (c) 2018 VectorNetworkProject. All rights reserved. MIT license.
+ *
+ * GitHub: https://github.com/VectorNetworkProject/TheMix
+ * Website: https://www.vector-network.tk
+ */
+
+namespace VectorNetworkProject\TheMix\event\game;
+
+
+use pocketmine\event\Cancellable;
+use pocketmine\event\player\PlayerEvent;
+use pocketmine\Player;
+
+class PlayerXpChangeEvent extends PlayerEvent implements Cancellable
+{
+    /* @var int $xp */
+    private $xp;
+
+    public function __construct(Player $player, int $xp)
+    {
+        $this->player = $player;
+        $this->xp = $xp;
+    }
+
+    /**
+     * 入手したXPを返します。
+     *
+     * @return int
+     */
+    public function getXp(): int
+    {
+        return $this->xp;
+    }
+}
