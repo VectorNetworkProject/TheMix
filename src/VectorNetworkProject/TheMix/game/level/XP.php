f380c20ce7d239124c1b600bcb689b0bcc272a89
InkoHX <inkohx@gmail.com>
2018-11-17 03:56:10 +0900

    Xp関連の完成

--- src/VectorNetworkProject/TheMix/game/level/XP.php
+++ src/VectorNetworkProject/TheMix/game/level/XP.php
@@ -9,34 +9,60 @@
 namespace VectorNetworkProject\TheMix\game\level;
 
 use pocketmine\Player;
+use pocketmine\Server;
+use VectorNetworkProject\TheMix\event\game\PlayerXpChangeEvent;
+use VectorNetworkProject\TheMix\provider\JSON;
 
 class XP
 {
+    /* @var string */
+    public const XP = 'xp';
+
+    /* @var string */
+    public const MAX = 'max';
+
     /**
      * @param Player $player
      * @param int $xp
+     *
      * @return void
      */
     public static function setXP(Player $player, int $xp): void
     {
-        //TODO:
+        $event = new PlayerXpChangeEvent($player, $xp);
+        Server::getInstance()->getPluginManager()->callEvent($event);
+        if (!$event->isCancelled()) {
+            $db = new JSON($player->getXuid(), Level::FILE_NAME);
+            $db->set(self::XP, $xp);
+        }
     }
 
     /**
      * @param Player $player
+     * @param int $min
+     * @param int $max
+     *
      * @return void
      */
-    public static function addXP(Player $player): void
+    public static function addXP(Player $player, int $min = 10, int $max = 15): void
     {
-        //TODO:
+        $xp = mt_rand($min, $max);
+        $event = new PlayerXpChangeEvent($player, $xp);
+        Server::getInstance()->getPluginManager()->callEvent($event);
+        if (!$event->isCancelled()) {
+            $db = new JSON($player->getXuid(), Level::FILE_NAME);
+            $db->set(self::XP, self::getXP($player) + $xp);
+        }
     }
 
     /**
      * @param Player $player
+     *
      * @return int
      */
     public static function getXP(Player $player): int
     {
-        //TODO:
+        $db = new JSON($player->getXuid(), Level::FILE_NAME);
+        return $db->get(self::XP);
     }
 }
