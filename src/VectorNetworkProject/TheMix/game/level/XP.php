5c24a35c32395ca9982d0888950f0b15226f60b0
InkoHX <inkohx@gmail.com>
2018-11-16 09:44:34 +0900

    xp

---
+++ src/VectorNetworkProject/TheMix/game/level/XP.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Copyright (c) 2018 VectorNetworkProject. All rights reserved. MIT license.
+ *
+ * GitHub: https://github.com/VectorNetworkProject/TheMix
+ * Website: https://www.vector-network.tk
+ */
+
+namespace VectorNetworkProject\TheMix\game\level;
+
+
+use pocketmine\Player;
+
+class XP
+{
+    /**
+     * @param Player $player
+     * @param int $xp
+     * @return void
+     */
+    public static function setXP(Player $player, int $xp): void
+    {
+        //TODO:
+    }
+
+    /**
+     * @param Player $player
+     * @return void
+     */
+    public static function addXP(Player $player): void
+    {
+        //TODO:
+    }
+
+    /**
+     * @param Player $player
+     * @return int
+     */
+    public static function getXP(Player $player): int
+    {
+        //TODO:
+    }
+}
