e3aaf91274d428771551bfe1ba280d87d9143d42
InkoHX <inkohx@gmail.com>
2018-11-21 20:29:07 +0900

    Add PlayerStreakEvent

---
+++ src/VectorNetworkProject/TheMix/game/event/player/PlayerBountyEvent.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright (c) 2018 VectorNetworkProject. All rights reserved. MIT license.
+ *
+ * GitHub: https://github.com/VectorNetworkProject/TheMix
+ * Website: https://www.vector-network.tk
+ */
+
+namespace VectorNetworkProject\TheMix\game\event\player;
+
+use pocketmine\event\Cancellable;
+use pocketmine\event\player\PlayerEvent;
+use pocketmine\Player;
+
+class PlayerBountyEvent extends PlayerEvent implements Cancellable
+{
+    /* @var int $gold */
+    private $gold;
+
+    /* @var bool $bounty */
+    private $bounty;
+
+    public function __construct(Player $player, int $gold, bool $bounty)
+    {
+        $this->player = $player;
+        $this->gold = $gold;
+        $this->bounty = $bounty;
+    }
+
+    /**
+     * @return int
+     */
+    public function getGold(): int
+    {
+        return $this->gold;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isBounty(): bool
+    {
+        return $this->bounty;
+    }
+}
