13b7d773fea8a37b9691a58e223cd54d55f92df8
InkoHX <inkohx@gmail.com>
2018-11-24 12:08:32 +0900

    Fix form

--- src/VectorNetworkProject/TheMix/command/defaults/ModeratorCommand.php
+++ src/VectorNetworkProject/TheMix/command/defaults/ModeratorCommand.php
@@ -73,9 +73,7 @@ class ModeratorCommand extends PluginCommand
     public static function sendLevelManagerUI(Player $player): void
     {
         $form = FormApi::makeListForm(function (Player $player, ?int $data) {
-            if (FormApi::formCancelled($data)) {
-                self::sendModeratorUI($player);
-            }
+            if (FormApi::formCancelled($data)) return;
             switch ($data) {
                 case 0:
                     $player->teleport(Server::getInstance()->getDefaultLevel()->getSpawnLocation());
