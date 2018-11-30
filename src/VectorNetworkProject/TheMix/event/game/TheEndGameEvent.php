45f46ff90a17c2bed9a5c504663d8f54ce380302
InkoHX <inkohx@gmail.com>
2018-12-01 05:18:17 +0900

    勝利報酬

--- src/VectorNetworkProject/TheMix/event/game/TheEndGameEvent.php
+++ src/VectorNetworkProject/TheMix/event/game/TheEndGameEvent.php
@@ -8,8 +8,11 @@
 
 namespace VectorNetworkProject\TheMix\event\game;
 
+use InkoHX\GoldLibrary\GoldAPI;
 use pocketmine\event\Listener;
 use pocketmine\Server;
+use VectorNetworkProject\TheMix\game\corepvp\blue\BlueTeamManager;
+use VectorNetworkProject\TheMix\game\corepvp\red\RedTeamManager;
 use VectorNetworkProject\TheMix\game\DefaultConfig;
 use VectorNetworkProject\TheMix\game\event\game\GameWinEvent;
 use VectorNetworkProject\TheMix\task\ResetGameTask;
@@ -23,6 +26,17 @@ class TheEndGameEvent implements Listener
             $event->setCancelled();
 
             return;
+        }
+        foreach (Server::getInstance()->getOnlinePlayers() as $player) {
+            if ($event->getType() === GameWinEvent::WIN_RED) {
+                if (RedTeamManager::isJoined($player)) {
+                    GoldAPI::addGold($player, 1000);
+                }
+            } else {
+                if (BlueTeamManager::isJoined($player)) {
+                    GoldAPI::addGold($player, 1000);
+                }
+            }
         }
         TheMix::getInstance()->getScheduler()->scheduleDelayedTask(new ResetGameTask(), 30 * 20);
         Server::getInstance()->broadcastTitle('§l§f===< §6決着 §f>===', '§aWin:§l '.$event->getType() === GameWinEvent::WIN_RED ? '§cRED' : '§bBLUE', 20, 5 * 20, 20);
