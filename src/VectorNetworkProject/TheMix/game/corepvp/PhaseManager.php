64b0a3270548e86631495aa16ef315e85cbf23e9
InkoHX <inkohx@gmail.com>
2018-12-18 18:54:57 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/VectorNetworkProject/TheMix/game/corepvp/PhaseManager.php
+++ src/VectorNetworkProject/TheMix/game/corepvp/PhaseManager.php
@@ -10,22 +10,23 @@
  * Created by PhpStorm.
  * User: InkoHX
  * Date: 2018/12/18
- * Time: 0:53
+ * Time: 0:53.
  */
 
 namespace VectorNetworkProject\TheMix\game\corepvp;
 
-
 class PhaseManager
 {
     /** @var int */
-    public const MAX_PHASE  = 5;
+    public const MAX_PHASE = 5;
     /** @var int $phase */
     private static $phase = 1;
 
     public static function addPhase(): void
     {
-        if (self::$phase <= self::MAX_PHASE) return;
+        if (self::$phase <= self::MAX_PHASE) {
+            return;
+        }
         self::$phase++;
     }
 
